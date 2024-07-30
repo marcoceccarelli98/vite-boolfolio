fc181e27f26b1dde66e662edd469b2b984551efb
marcoceccarelli98 <mceccarelli98@gmail.com>
2024-07-30 23:39:08 +0200

    [Milestone 5]

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <script>
 import axios from "axios";
+import AppCard from "./components/AppCard.vue";
 
 export default {
   data() {
@@ -8,13 +9,17 @@ export default {
     };
   },
 
+  components: {
+    AppCard,
+  },
+
   methods: {
     async getProjects() {
       try {
         const response = await axios.get("http://127.0.0.1:8000/api/projects");
 
         this.projects = response.data.results;
-        console.log(response.data);
+        console.log(this.projects);
       } catch (error) {
         console.error("Error fetching projects:", error);
       }
@@ -29,7 +34,16 @@ export default {
 
 <template>
   <ul>
-    <li v-for="project in this.projects">{{ project.title }}</li>
+    <li v-for="project in this.projects">
+      <AppCard
+        :title="project.title"
+        :description="project.description"
+        :status="project.status"
+        :start_date="project.start_date"
+        :end_date="project.end_date"
+        :image="project.images[0]"
+      ></AppCard>
+    </li>
   </ul>
 </template>
 
---
+++ src/components/AppCard.vue
@@ -0,0 +1,128 @@
+<script>
+export default {
+  name: "AppCard",
+  components: {},
+
+  props: {
+    title: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    status: {
+      type: String,
+      required: true,
+    },
+    start_date: {
+      type: String,
+      required: false,
+    },
+    end_date: {
+      type: String,
+      required: false,
+    },
+    image: {
+      type: String,
+      required: false,
+    },
+    data() {
+      return {};
+    },
+  },
+
+  // METHODS
+  methods: {},
+};
+</script>
+
+<template>
+  <div class="card-container">
+    <!-- POSTER -->
+    <div class="image-content">
+      <img
+        v-if="image"
+        class="poster-image"
+        :src="image"
+        :alt="title + ' poster'"
+      />
+      <div class="noImage" v-else>IMAGE NOT FOUND</div>
+    </div>
+    <!-- DETAILS -->
+    <ul class="details">
+      <!-- TITLE -->
+      <li>
+        <h3 class="mb-10">{{ title }}</h3>
+      </li>
+      <!-- /TITLE -->
+      <!-- DESCRIPTION -->
+      <li>
+        <p class="mb-15">{{ description }}</p>
+      </li>
+      <!-- /DESCRIPTION -->
+      <!-- STATUS -->
+      <li>
+        <p>{{ status }}</p>
+      </li>
+      <!-- /STATUS -->
+      <!-- START DATE -->
+      <li v-if="status !== 'To Do'" class="mb-15">{{ start_date }}</li>
+      <!-- /START DATE -->
+      <!-- END DATE -->
+      <li v-if="status === 'Done'" class="mb-15">{{ end_date }}</li>
+      <!-- /END DATE -->
+    </ul>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.card-container {
+  width: 514px;
+  height: 514px;
+  border: 1px solid white;
+  background-color: black;
+  color: white;
+  overflow-y: hidden;
+  // HOVER EFFECT
+  &:hover {
+    .poster-image,
+    .noImage {
+      // display: none;
+      opacity: 0;
+      max-height: 0;
+      transition: 0.5s;
+    }
+    .details {
+      opacity: 1;
+      max-height: 600px;
+      transition: 0.3s;
+      display: inline-block;
+    }
+  }
+}
+
+.image-content {
+  transition: opacity 0.3s, max-height 0.3s;
+  overflow: hidden;
+  .poster-image {
+    position: relative;
+    width: 512px;
+    height: 512px;
+  }
+}
+
+// TEXT DETAILS
+.details {
+  color: white;
+  padding: 30px;
+  //display: none;
+  .text-list {
+    line-height: 20px;
+    li:first-of-type {
+      margin-top: 5px;
+    }
+  }
+}
+</style>
