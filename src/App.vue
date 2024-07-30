abedad66801b56d843c144988e1ba7c039cbab0d
marcoceccarelli98 <mceccarelli98@gmail.com>
2024-07-30 16:06:47 +0200

    [Milestone 3]

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,20 @@
-<script></script>
+<script>
+import axios from "axios";
+
+export default{
+
+data(){
+
+}
+
+methods:{
+    getProjects(){
+        axios.get('http://127.0.0.1:8000/api/projects')
+    }
+}
+
+}
+</script>
 
 <template></template>
 
